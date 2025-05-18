94e948aba2be4131424ceca3876fc3807fd22716
NULLderef <redmastersuper@gmail.com>
2025-05-18 09:43:57 +0200

    wlroots-0.20 build changes, wlr private api refactoring, etc.

--- render/fx_renderer/fx_pass.c
+++ render/fx_renderer/fx_pass.c
@@ -6,7 +6,6 @@
 #include <unistd.h>
 #include <wlr/render/allocator.h>
 #include <wlr/render/drm_syncobj.h>
-#include <wlr/types/wlr_matrix.h>
 #include <wlr/util/transform.h>
 #include <wlr/util/log.h>
 #include <wlr/util/region.h>
@@ -238,29 +237,29 @@ static void render(const struct wlr_box *box, const pixman_region32_t *clip, GLi
 
 static void set_proj_matrix(GLint loc, float proj[9], const struct wlr_box *box) {
 	float gl_matrix[9];
-	wlr_matrix_identity(gl_matrix);
-	wlr_matrix_translate(gl_matrix, box->x, box->y);
-	wlr_matrix_scale(gl_matrix, box->width, box->height);
-	wlr_matrix_multiply(gl_matrix, proj, gl_matrix);
+	matrix_identity(gl_matrix);
+	matrix_translate(gl_matrix, box->x, box->y);
+	matrix_scale(gl_matrix, box->width, box->height);
+	matrix_multiply(gl_matrix, proj, gl_matrix);
 	glUniformMatrix3fv(loc, 1, GL_FALSE, gl_matrix);
 }
 
 static void set_tex_matrix(GLint loc, enum wl_output_transform trans,
 		const struct wlr_fbox *box) {
 	float tex_matrix[9];
-	wlr_matrix_identity(tex_matrix);
-	wlr_matrix_translate(tex_matrix, box->x, box->y);
-	wlr_matrix_scale(tex_matrix, box->width, box->height);
-	wlr_matrix_translate(tex_matrix, .5, .5);
+	matrix_identity(tex_matrix);
+	matrix_translate(tex_matrix, box->x, box->y);
+	matrix_scale(tex_matrix, box->width, box->height);
+	matrix_translate(tex_matrix, .5, .5);
 
 	// since textures have a different origin point we have to transform
 	// differently if we are rotating
 	if (trans & WL_OUTPUT_TRANSFORM_90) {
-		wlr_matrix_transform(tex_matrix, wlr_output_transform_invert(trans));
+		matrix_transform(tex_matrix, wlr_output_transform_invert(trans));
 	} else {
-		wlr_matrix_transform(tex_matrix, trans);
+		matrix_transform(tex_matrix, trans);
 	}
-	wlr_matrix_translate(tex_matrix, -.5, -.5);
+	matrix_translate(tex_matrix, -.5, -.5);
 
 	glUniformMatrix3fv(loc, 1, GL_FALSE, tex_matrix);
 }
--- util/matrix.c
+++ util/matrix.c
@@ -68,3 +68,75 @@ void matrix_projection(float mat[static 9], int width, int height,
 	// Identity
 	mat[8] = 1.0f;
 }
+
+void matrix_identity(float mat[static 9]) {
+	static const float identity[9] = {
+		1.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 1.0f,
+	};
+	memcpy(mat, identity, sizeof(identity));
+}
+
+void matrix_multiply(float mat[static 9], const float a[static 9],
+		const float b[static 9]) {
+	float product[9];
+
+	product[0] = a[0]*b[0] + a[1]*b[3] + a[2]*b[6];
+	product[1] = a[0]*b[1] + a[1]*b[4] + a[2]*b[7];
+	product[2] = a[0]*b[2] + a[1]*b[5] + a[2]*b[8];
+
+	product[3] = a[3]*b[0] + a[4]*b[3] + a[5]*b[6];
+	product[4] = a[3]*b[1] + a[4]*b[4] + a[5]*b[7];
+	product[5] = a[3]*b[2] + a[4]*b[5] + a[5]*b[8];
+
+	product[6] = a[6]*b[0] + a[7]*b[3] + a[8]*b[6];
+	product[7] = a[6]*b[1] + a[7]*b[4] + a[8]*b[7];
+	product[8] = a[6]*b[2] + a[7]*b[5] + a[8]*b[8];
+
+	memcpy(mat, product, sizeof(product));
+}
+
+void matrix_translate(float mat[static 9], float x, float y) {
+	float translate[9] = {
+		1.0f, 0.0f, x,
+		0.0f, 1.0f, y,
+		0.0f, 0.0f, 1.0f,
+	};
+	matrix_multiply(mat, mat, translate);
+}
+
+void matrix_scale(float mat[static 9], float x, float y) {
+	float scale[9] = {
+		x,    0.0f, 0.0f,
+		0.0f, y,    0.0f,
+		0.0f, 0.0f, 1.0f,
+	};
+	matrix_multiply(mat, mat, scale);
+}
+
+void matrix_transform(float mat[static 9],
+		enum wl_output_transform transform) {
+	matrix_multiply(mat, mat, transforms[transform]);
+}
+
+void matrix_project_box(float mat[static 9], const struct wlr_box *box,
+		enum wl_output_transform transform, const float projection[static 9]) {
+	int x = box->x;
+	int y = box->y;
+	int width = box->width;
+	int height = box->height;
+
+	matrix_identity(mat);
+	matrix_translate(mat, x, y);
+
+	matrix_scale(mat, width, height);
+
+	if (transform != WL_OUTPUT_TRANSFORM_NORMAL) {
+		matrix_translate(mat, 0.5, 0.5);
+		matrix_transform(mat, transform);
+		matrix_translate(mat, -0.5, -0.5);
+	}
+
+	matrix_multiply(mat, projection, mat);
+}
